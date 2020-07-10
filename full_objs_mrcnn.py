7c011fc927147856b2bf338c98aa32b19f1b4604
abelttitus <abelttitus1998@gmail.com>
2020-07-10 13:52:08 +0530

    2

--- full_objs_mrcnn.py
+++ full_objs_mrcnn.py
@@ -58,57 +58,57 @@ if __name__ == "__main__":
   print("Initializing voxel volume...")
   class_ids= [29,33,40,41,63]
   
-  for index in class_ids:
-      tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.005)
+  class_id=29
+  tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.005)
+
+
+  t0_elapse = time.time()
+
+  file=open('data_table/associate.txt')
+  lines = file.read().split("\n")
+  for i in range(len(lines)-1):
+    rgb_file=base_dir+'/'+lines[i].split(" ")[1]
+    depth_file=base_dir+'/'+lines[i].split(" ")[2]
+    print("Fusing frame %d/%d"%(i+1, n_imgs))
+    img_no=lines[i].split(" ")[1].split("_")[1][:4]
+    final_mask=np.zeros((480,640,3),dtype=np.bool)
+    mask_index_file=open(mask_dir+img_no+'.txt')
+    mask_index_file_lines = mask_index_file.read().split("\n")
+    flag=False
+    for j in range(len(mask_index_file_lines)-1):
+        contents=mask_index_file_lines[j].split(" ")
+        if int(contents[1]) != class_id:
+            continue
+        flag=True
+        mfile=contents[0]
+        mask=cv2.imread(mfile)
+        mask=(mask-mask.min())/(mask.max()-mask.min())
+        mask=mask.astype(np.bool)
+        final_mask=np.logical_or(mask,final_mask)
+        # Read RGB-D image and camera pose
+    mask_index_file.close()
+    if flag:
+        final_mask=final_mask.astype(np.float)
+        color_image = cv2.cvtColor(cv2.imread(rgb_file),cv2.COLOR_BGR2RGB)
+        color_image=(color_image*final_mask).astype(np.uint8)
+        depth_im = cv2.imread(depth_file,cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        depth_im=(depth_im*final_mask[:,:,0]).astype(np.float)
+        depth_im /= 5000.
+          #depth_im[depth_im == 65.535] = 0
+        img_no_int=int(img_no)
+        cam_pose=cam_poses[4*img_no_int:4*(img_no_int+1),:]
     
     
-      t0_elapse = time.time()
-    
-      file=open('data_table/associate.txt')
-      lines = file.read().split("\n")
-      for i in range(len(lines)-1):
-        rgb_file=base_dir+'/'+lines[i].split(" ")[1]
-        depth_file=base_dir+'/'+lines[i].split(" ")[2]
-        print("Fusing frame %d/%d"%(i+1, n_imgs))
-        img_no=lines[i].split(" ")[1].split("_")[1][:4]
-        final_mask=np.zeros((480,640,3),dtype=np.bool)
-        mask_index_file=open(mask_dir+img_no+'.txt')
-        mask_index_file_lines = mask_index_file.read().split("\n")
-        flag=False
-        for j in range(len(mask_index_file_lines)-1):
-            contents=mask_index_file_lines[j].split(" ")
-            if int(contents[1]) != class_ids[index]:
-                continue
-            flag=True
-            mfile=contents[0]
-            mask=cv2.imread(mfile)
-            mask=(mask-mask.min())/(mask.max()-mask.min())
-            mask=mask.astype(np.bool)
-            final_mask=np.logical_or(mask,final_mask)
-            # Read RGB-D image and camera pose
-        mask_index_file.close()
-        if flag:
-            final_mask=final_mask.astype(np.float)
-            color_image = cv2.cvtColor(cv2.imread(rgb_file),cv2.COLOR_BGR2RGB)
-            color_image=(color_image*final_mask).astype(np.uint8)
-            depth_im = cv2.imread(depth_file,cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-            depth_im=(depth_im*final_mask[:,:,0]).astype(np.float)
-            depth_im /= 5000.
-              #depth_im[depth_im == 65.535] = 0
-            img_no_int=int(img_no)
-            cam_pose=cam_poses[4*img_no_int:4*(img_no_int+1),:]
-        
-        
-              # Integrate observation into voxel volume (assume color aligned with depth)
-            tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
-         
-      file.close()
-      fps = n_imgs / (time.time() - t0_elapse)
-      print("Average FPS: {:.2f}".format(fps))
-    
-    # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
-      mesh_str=str(class_ids[index])
-      print("Saving mesh to mesh.ply...")
-      verts, faces, norms, colors = tsdf_vol.get_mesh()
-      fusion.meshwrite(mesh_str+'.ply', verts, faces, norms, colors)
-      del tsdf_vol
+          # Integrate observation into voxel volume (assume color aligned with depth)
+        tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
+     
+  file.close()
+  fps = n_imgs / (time.time() - t0_elapse)
+  print("Average FPS: {:.2f}".format(fps))
+
+# Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
+  mesh_str=str(class_id)
+  print("Saving mesh to mesh.ply...")
+  verts, faces, norms, colors = tsdf_vol.get_mesh()
+  fusion.meshwrite(mesh_str+'.ply', verts, faces, norms, colors)
+      
