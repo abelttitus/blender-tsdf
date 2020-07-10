1674cf6aed240986dca72b4807053b674d845fc9
abelttitus <abelttitus1998@gmail.com>
2020-07-10 13:59:54 +0530

    6

--- full_objs_mrcnn.py
+++ full_objs_mrcnn.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
   print("Initializing voxel volume...")
   class_ids= [29,33,40,41,63]
   
-  for index in class_ids:
+  for index in range(len(class_ids)):
       tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.01)
     
     
